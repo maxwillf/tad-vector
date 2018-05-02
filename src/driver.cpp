9a19c78cd06fc55aa9c05a08244a7745163e9a79
Felipe Ramos <felipecramos@ufrn.edu.br>
2018-05-01 22:57:41 -0300

    Special members in progress

--- src/driver.cpp
+++ src/driver.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <chrono>
 #include "vector.hpp"
+#include <vector>
 
 /* Client code functions {{{*/
 namespace client
@@ -10,6 +11,14 @@ namespace client
 	// Simple array printer
 	template <class T>
 		void print_it( sc::Vector<T> &V, char sep ){
+			for( auto it = V.begin(); it != V.end(); ++it ){
+				std::cout << *it << sep;
+			}
+			std::cout << std::endl;
+		}
+
+	template <class T>
+		void print_it_vec( std::vector<T> &V, char sep ){
 			for( auto it = V.begin(); it != V.end(); ++it ){
 				std::cout << *it << sep;
 			}
@@ -42,6 +51,11 @@ namespace h{
 		std::cout << "~ " << phrase;
 		std::cout << "\e[0m" << std::endl;
 	}
+	
+	void h2(std::string phrase){
+		std::cout << "\n> ";
+		std::cout << phrase << std::endl;
+	}
 }
 /*}}}*/
 
@@ -50,7 +64,60 @@ int main( void ){
 	/* Special members tests {{{*/
 	{
 		h::h1("Special members tests");
-		std::cout << "TESTS HERE...\n";
+
+		{	h::h2("Empty constructor");
+			
+			// Declaration
+			sc::Vector<int> EmptyVector; 
+			std::cout << "EmptyVector object created!\n";
+
+			// Populating
+			std::cout << "Populating EmptyVector with push_back()...\n";
+			for( int i = 0; i < 10; i++ )
+				EmptyVector.push_back(i);
+
+			// Simple print the object
+			std::cout << "EmptyVector elements: ";
+			client::print_it(EmptyVector, ' ');
+		}
+
+		{	h::h2("Constructor with pre-defined size");
+
+			// Declaration
+			sc::Vector<int> SizeVector(10);
+
+			// Populating
+			std::cout << "Populating SizeVector with positive values...\n";
+			for( int i = 0; i < 10; i++ )
+				SizeVector[i] = i;
+
+			// Simple print the object
+			std::cout << "SizeVector elements: ";
+			client::print_it( SizeVector, ' ' );
+		}
+
+		{	h::h2("Copy constructor from a simple array");
+
+			// Declaration
+			int Vet[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			sc::Vector<int> cVector( Vet, Vet+10 );
+
+			// Simple print
+			std::cout << "Copy vector elements: ";
+			client::print_it( cVector, ' ' );
+		}
+		
+		{	h::h2("Copy constructor from another sc::Vector");
+
+			// Declaration
+			sc::Vector<int> origVec(10);
+			client::populate( origVec );
+			sc::Vector<int> copyVec( origVec );
+
+			// Simple print
+			std::cout << "Copied vector elements: ";
+			client::print_it( copyVec, ' ' );
+		}
 		h::sep();
 	}
 	/*}}}*/
