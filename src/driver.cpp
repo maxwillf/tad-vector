f19f94f3a439e9d9cf55f58dad51372a033e6937
Felipe Ramos <felipecramos@ufrn.edu.br>
2018-05-01 22:11:17 -0300

    Driver code started

---
+++ src/driver.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include "vector.hpp"
+
+/* Client code functions {{{*/
+namespace client
+{
+	// Here, all the client-side functions are defined
+	// Simple array printer
+	template <class T>
+		void print_it( sc::Vector<T> &V, char sep ){
+			for( auto it = V.begin(); it != V.end(); ++it ){
+				std::cout << *it << sep;
+			}
+			std::cout << std::endl;
+		}
+
+	// Populate a given array with random values between [0, 1000)
+	template <class T>
+		void populate( sc::Vector<T> &V ){
+			for( auto it = V.begin(); it != V.end(); ++it ){
+				auto seed = 
+					std::chrono::system_clock::now().time_since_epoch().count();
+				std::mt19937 rd (seed);
+				*it = rd() % 1000;
+			}
+		}
+}
+/*}}}*/
+
+/* Styles functions {{{*/
+namespace h{
+	void sep( void ){
+		for( int i = 0; i < 80; i++ )
+			std::cout << "-";
+		std::cout << std::endl;
+	}
+
+	void h1(std::string phrase){
+		std::cout << "\e[1m";
+		std::cout << "~ " << phrase;
+		std::cout << "\e[0m" << std::endl;
+	}
+}
+/*}}}*/
+
+int main( void ){
+
+	/* Special members tests {{{*/
+	{
+		h::h1("Special members tests");
+		std::cout << "TESTS HERE...\n";
+		h::sep();
+	}
+	/*}}}*/
+	
+	/* Capacity functions tests {{{*/
+	{
+		h::h1("Capacity functions tests");
+		std::cout << "TESTS HERE...\n";
+		h::sep();
+	}
+	/*}}}*/
+
+	/* Modifiers functions tests {{{*/
+	{
+		h::h1("Modifiers functions tests");
+		std::cout << "TESTS HERE...\n";
+		h::sep();
+	}
+	/*}}}*/
+
+	/* Element access functions tests {{{*/
+	{
+		h::h1("Element access functions tests");
+		std::cout << "TESTS HERE...\n";
+		h::sep();
+	}
+	/*}}}*/
+
+	/* Operators tests {{{*/
+	{
+		h::h1("Operators tests");
+		std::cout << "TESTS HERE...\n";
+		h::sep();
+	}
+	/*}}}*/
+
+	/* Iterators tests {{{*/
+	{
+		h::h1("Iterators tests");
+		std::cout << "TESTS HERE...\n";
+		h::sep();
+	}
+	/*}}}*/
+
+	return 0;
+}
